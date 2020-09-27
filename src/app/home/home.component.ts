5d776aeb4a85e24a1380080977b541af05cfaf3d
Tim Robertson <trobertson@ineosolutionsinc.com>
2020-09-27 01:32:50 -0700

    Add bootstrap, rpm by load table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+const MAX_RPM = 7200;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,8 +10,32 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  loadCells;
+  rpmCells;
+
+  cellHits = {};
+
+  constructor(private router: Router) { 
+    this.loadCells = Array(11).fill(1).map((x, i) => i * 10);
+    this.rpmCells = [];
+    for (let rpm = 500; rpm < MAX_RPM; rpm += 250) {
+      this.rpmCells.push(rpm);
+      this.cellHits[rpm] = {};
+      this.loadCells.forEach(load => {
+        this.cellHits[rpm][load] = [];
+      });
+    }
+  }
 
   ngOnInit(): void { }
+
+  getCellHit(rpm: number, load: number) {
+    const hit = this.cellHits[rpm][load];
+    if (hit.length === 0) {
+      return 0;
+    }
+
+    return 1;
+  }
 
 }
