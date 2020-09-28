42e9dcbae6393b88c9d9907f236261f649ead9b8
Tim Robertson <timr@sfu.ca>
2020-09-27 18:17:19 -0700

    Connects to UTEC, AEM UEGO. Table updates cell hits

--- src/app/core/services/serialport/serialport.service.ts
+++ src/app/core/services/serialport/serialport.service.ts
@@ -6,6 +6,21 @@ import { Injectable } from '@angular/core';
 import * as serialport from 'serialport';
 import * as serialport_readline from '@serialport/parser-readline';
 
+import { Observable } from 'rxjs';
+
+export class UTECValues {
+  constructor(rpm: number, load: number) {
+    this.rpm = rpm;
+    this.load = load;
+  }
+  rpm: number;
+  load: number;
+
+  toString() {
+    return `RPM: ${this.rpm}, LOAD: ${this.load}`;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,6 +32,9 @@ export class SerialPortService {
   get isElectron(): boolean {
     return !!(window && window.process && window.process.type);
   }
+
+  utecObservable: Observable<UTECValues> = null;
+  afrGaugeObservable: Observable<number> = null;
 
 // ---------AEM UEGO
 
@@ -49,34 +67,44 @@ export class SerialPortService {
         console.log("ports", data);
       })
 
-      // debug afr
-      // const port = new this.SerialPort('COM3', { baudRate: 9600 });
-      // const parser = port.pipe(new this.Readline({delimiter: '\n'}));
-      // parser.on('data', console.log);
-      
-      // debug utec
-      const port = new this.SerialPort('COM4', { baudRate: 19200 }, (err) => {
-        if (err) {
-          return console.log('Error opening COM4: ', err.message)
-        }
-      });
-
-      port.write('1', (err) => {
-        if (err) {
-          return console.log('Error entering UTEC Logger');
-        }
+      // AFR Gauge
+      this.afrGaugeObservable = new Observable<number>((subscriber) => {
+        const port = new this.SerialPort('COM3', { baudRate: 9600 });
+        const parser = port.pipe(new this.Readline({delimiter: '\n'}));
+        parser.on('data', (data) => {
+          subscriber.next(+data);
+        });
       })
 
-      //  RPM   MAP  MAF TPS Site Count   Inj#1 Ign#1  Inj#1  Ign   Fuel     MAF  
-      const parser = port.pipe(new this.Readline({delimiter: '\n'}));
-      parser.on('data', (data) => {
-        // console.log(data);
-        const dataCleaned = data.replace(/ +(?= )/g,'');
-        const split = dataCleaned.trim().split(' ');
-        const rpm = split[0];
-        const load = split[4];
-        console.log('RPM / LOAD', rpm, load);
-      });
+      // UTEC
+      this.utecObservable = new Observable<UTECValues>((subscriber) => {
+ 
+        const port = new this.SerialPort('COM4', { baudRate: 19200 }, (err) => {
+          if (err) {
+            return console.log('Error opening COM4: ', err.message)
+          }
+        });
+
+        // Enter UTEC Logger mode 1
+        port.write('1', (err) => {
+          if (err) {
+            return console.log('Error entering UTEC Logger');
+          }
+        })
+
+        //  RPM   MAP  MAF TPS Site Count   Inj#1 Ign#1  Inj#1  Ign   Fuel     MAF  
+        const parser = port.pipe(new this.Readline({delimiter: '\n'}));
+        parser.on('data', (data) => {
+          // console.log(data);
+          const dataCleaned = data.replace(/ +(?= )/g,'');
+          const split = dataCleaned.trim().split(' ');
+          const rpm = +split[0];
+          const load = +split[4];
+          // console.log('RPM / LOAD', rpm, load);
+          subscriber.next(new UTECValues(rpm, load));
+        });
+      })
+     
 
     }
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { ElectronService, SerialPortService } from 'app/core/services';
+import { combineLatest, Subscription } from 'rxjs';
 
 const MAX_RPM = 7200;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   loadCells;
   rpmCells;
 
   cellHits = {};
 
+  dataSubscription: Subscription = null;
+
   constructor(
-    private router: Router,
+    private cdr: ChangeDetectorRef,
     private electronService: ElectronService,
     private serialPortService: SerialPortService
     ) { 
@@ -32,7 +35,37 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.electronService.isElectron) {
+      this.dataSubscription = combineLatest([this.serialPortService.utecObservable, this.serialPortService.afrGaugeObservable])
+      .subscribe((data) => {
+
+        // If load data is not between 0 - 100 then it is erroneous
+        if ((data[0].load - Math.floor(data[0].load)) !== 0) {
+          return;
+        }
+
+        // Get closest rpm cell to the current rpm 
+        // e.g. 765 rpm -> 750 rpm cell
+        const closestRpm = this.closest(data[0].rpm, this.rpmCells);
+
+        console.log("data", data[0], closestRpm);
+
+        // Count cell hit for RPM, Load and AFR
+        this.cellHits[closestRpm][data[0].load].push(data[1]);
+
+        // Update angular view
+        this.cdr.detectChanges();
+      })
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+      this.dataSubscription = null;
+    }
+  }
 
   getCellHit(rpm: number, load: number) {
     const hit = this.cellHits[rpm][load];
@@ -40,7 +73,26 @@ export class HomeComponent implements OnInit {
       return 0;
     }
 
-    return 1;
+    return hit.length;
   }
+
+  // closest algorithm by paxdiablo: https://stackoverflow.com/a/8584940
+  closest = (num: number, arr: number[]) => {
+    let mid;
+    let lo = 0;
+    let hi = arr.length - 1;
+    while (hi - lo > 1) {
+        mid = Math.floor ((lo + hi) / 2);
+        if (arr[mid] < num) {
+            lo = mid;
+        } else {
+            hi = mid;
+        }
+    }
+    if (num - arr[lo] <= arr[hi] - num) {
+        return arr[lo];
+    }
+    return arr[hi];
+}
 
 }
